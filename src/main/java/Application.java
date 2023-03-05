c0d63efca6e32f990b5266a739d2996102bd24d2
Dianakyz <3222777my@gmail.com>
2023-03-05 12:20:02 +0300

    first commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+import java.sql.SQLException;
+
+public class Application {
+
+    public static void main(String[] args) throws SQLException {
+
+        final String user = "postgres";
+        final String password = "MASA858chu";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+    }
+}
